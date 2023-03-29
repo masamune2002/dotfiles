53061d945b542da91b6ad80bcdadd2f184061469
Alex Ingraham <masamune2002@gmail.com>
2023-03-29 16:08:04 -0500

    Add remaining keymaps from original vim config

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,4 +14,16 @@ vim.g.maplocalleader = " "
 keymap("n", "<leader>h", "<C-w>h", opts)
 keymap("n", "<leader>j", "<C-w>j", opts)
 keymap("n", "<leader>k", "<C-w>k", opts)
-keymap("n", "<leader>l", "<C-w>l", opts)
+
+-- Leader Commands
+keymap('n', '<leader>pi', ':PlugInstall<CR>', opts)
+keymap('n', '<leader>u', ':UndotreeToggle<CR> ', opts)
+keymap('n', '<leader>n', ':NERDTreeToggle<CR>', opts)
+keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
+keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
+keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
+keymap('n', '<leader>oc', '<cmd>:sp ~/.config/nvim/cheat-sheet.txt<CR>', opts)
+keymap('n', '<leader>ov', '<cmd>:sp ~/.config/nvim/init.vim<cr>', opts)
+keymap('n', '<leader>sv', ':source $MYVIMRC<CR>', opts)
+
