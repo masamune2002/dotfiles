f09b2a4f91e1ceb26767d1a043642be37e51ef53
Alex Ingraham <masamune2002@gmail.com>
2024-08-18 16:26:05 -0500

    put back font stuff

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ cat apps.list | xargs sudo dnf -y install
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
+sudo cp fonts/JetBrainsMono-2.304/fonts/ttf/* /usr/share/fonts
+
+fc-cache -f -v
+
